549b4d3d53b95a6ed098f027bdc1200b8156b667
CrazyBene <benedikt.grau@gmx.de>
2024-12-06 14:33:17 +0100

    Add solution for day 6

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Advent Of Code 2024
 
-![Days completed](https://img.shields.io/badge/Days%20Completed-5-green)
-![Stars obtained](https://img.shields.io/badge/Stars%20Obtained%20⭐-10-yellow)
+![Days completed](https://img.shields.io/badge/Days%20Completed-6-green)
+![Stars obtained](https://img.shields.io/badge/Stars%20Obtained%20⭐-12-yellow)
 ![Help Needed](https://img.shields.io/badge/Help%20Needed%20➕-0-purple)
 ![Not Solved](https://img.shields.io/badge/Not%20Solved%20❌-0-red)
 
@@ -16,6 +16,7 @@ These are my solution to the Advent of Code[^aoc-footnote] puzzles of 2024.
 | 03  |    [Mull It Over][day03]    |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/Day03.kt) |
 | 04  |    [Ceres Search][day04]    |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/Day04.kt) |
 | 05  |    [Print Queue][day05]     |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/Day05.kt) |
+| 06  |  [Guard Gallivant][day06]   |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/Day06.kt) |
 
 [^aoc-footnote]:
 [Advent of Code][aoc] – An annual event of Christmas-oriented programming challenges started December 2015.
@@ -34,3 +35,5 @@ You can solve the puzzle and provide an answer using the language of your choice
 [day04]: https://adventofcode.com/2024/day/4
 
 [day05]: https://adventofcode.com/2024/day/5
+
+[day06]: https://adventofcode.com/2024/day/6
---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,108 @@
+import kotlin.collections.plusAssign
+
+fun main() = Day06.run(RunMode.BOTH)
+
+object Day06 : BasicDay() {
+
+    override val expectedTestValuePart1 = 41
+    override val expectedTestValuePart2 = 6
+
+    override val solvePart1: ((List<String>) -> Int) = { input ->
+        val width = input.first().length
+        val height = input.size
+
+        val (obstructions, startPosition) = parseInput(input)
+
+        val placesVisited = walkThroughLab(startPosition, north, obstructions, width, height).first
+        placesVisited.count()
+    }
+
+    override val solvePart2: ((List<String>) -> Int) = { input ->
+        val width = input.first().length
+        val height = input.size
+
+        val (obstructions, startPosition) = parseInput(input)
+
+        val placesVisited = walkThroughLab(startPosition, north, obstructions, width, height).first
+        val possibleObstructions = placesVisited.toMutableSet().apply { remove(startPosition) }.toSet()
+
+        possibleObstructions.filter { possibleObstruction ->
+            val obstructionsToTest = obstructions.toMutableSet().apply { add(possibleObstruction) }.toSet()
+            walkThroughLab(startPosition, north, obstructionsToTest, width, height).second
+        }.count()
+    }
+
+    private fun parseInput(input: List<String>): Pair<Set<Position>, Position> {
+        val obstructions = mutableSetOf<Position>()
+        var startPosition: Position? = null
+
+        input.forEachIndexed { row, line ->
+            line.forEachIndexed { col, char ->
+                when (char) {
+                    '#' -> obstructions += Position(col, row)
+                    '^' -> startPosition = Position(col, row)
+                }
+            }
+        }
+
+        if (startPosition == null)
+            error("No startPosition found.")
+
+        return obstructions to startPosition
+    }
+
+    private data class Position(val x: Int, val y: Int)
+    private data class Direction(val x: Int, val y: Int)
+
+    private val north = Direction(0, -1)
+    private val east = Direction(1, 0)
+    private val south = Direction(0, 1)
+    private val west = Direction(-1, 0)
+
+    private fun getNextPosition(currentPosition: Position, currentDirection: Direction) =
+        Position(currentPosition.x + currentDirection.x, currentPosition.y + currentDirection.y)
+
+    private fun getNextDirection(currentDirection: Direction): Direction {
+        return when (currentDirection) {
+            north -> east
+            east -> south
+            south -> west
+            west -> north
+            else -> error("Unknown direction.")
+        }
+    }
+
+    private fun isInBounds(position: Position, width: Int, height: Int): Boolean {
+        return position.x in 0..<width && position.y in 0..<height
+    }
+
+    private fun walkThroughLab(
+        guardStartPosition: Position,
+        guardStartDirection: Direction,
+        obstructions: Set<Position>,
+        width: Int,
+        height: Int
+    ): Pair<Collection<Position>, Boolean> {
+        var guardPosition = guardStartPosition
+        var guardDirection = guardStartDirection
+        var placesVisited = mutableSetOf<Pair<Position, Direction>>()
+
+        while (isInBounds(guardPosition, width, height)) {
+            placesVisited += guardPosition to guardDirection
+
+            var possibleNextPosition = getNextPosition(guardPosition, guardDirection)
+            while (possibleNextPosition in obstructions) {
+                guardDirection = getNextDirection(guardDirection)
+                possibleNextPosition = getNextPosition(guardPosition, guardDirection)
+            }
+            guardPosition = possibleNextPosition
+
+            if (guardPosition to guardDirection in placesVisited) {
+                return placesVisited.map { it.first }.distinct() to true
+            }
+        }
+
+        return placesVisited.map { it.first }.distinct() to false
+    }
+
+}
